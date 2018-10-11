b17225dec6668f65648d022e226c99e0f9e8acce
marifer <mafevito@hotmail.com>
2018-10-11 23:55:47 +0200

    cambios

--- 04-html-editor-preview/html-editor-preview/src/App.js
+++ 04-html-editor-preview/html-editor-preview/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HelloWorld from 'Components/Editor';
+
 class App extends Component {
   render() {
     return (
