3bc3b41b9bc3980d16ca9c0731c67e66381f1c89
marifer <mafevito@hotmail.com>
2018-10-16 23:32:28 +0200

    work on pomodoro

--- 04-PomodoroTimer/pomodoro/src/Components/Timer.js
+++ 04-PomodoroTimer/pomodoro/src/Components/Timer.js
@@ -1,8 +1,34 @@
 import React, {Component} from 'react';
 
 class Timer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            secondsElapsed: 65
+        }
+
+        this.handleStartClick = this.handleStartClick.bind(this);
+    }
+    
+    getMinutes() {
+        return Math.floor(this.state.secondsElapsed / 60);
+    }
+
+    getSeconds() {
+        return('0' + this.state.secondsElapsed % 60).slice(-2);
+    }
+
     handleStartClick() {
-        alert("starting..");
+        //alert("starting..");
+
+        setInterval(function() {
+            this.setState((prevState) => {
+                return {
+                    secondsElapsed: prevState.secondsElapsed + 1
+                }
+            })
+        }, 1000)
     }
 
     handleStopClick() {
@@ -12,7 +38,7 @@ class Timer extends Component {
     render() {
         return (
             <div>
-            <h1>0:00</h1>
+            <h1>{this.getMinutes()}:{this.getSeconds()}</h1>
 
             <button type="button" onClick={this.handleStartClick}>start</button>
             <button type="button" onClick={this.handleStopClick}>stop</button>
